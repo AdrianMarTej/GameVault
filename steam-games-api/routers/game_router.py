a697fb720dbe43004178c325ef2ed52b70e951ce
AdrianMarTej <adrian.martinez.t@opendeusto.es>
2024-12-22 13:05:33 +0100

    game router fix

--- steam-games-api/routers/game_router.py
+++ steam-games-api/routers/game_router.py
@@ -1,27 +1,28 @@
-from fastapi import apirouter, HTTPException
+from fastapi import APIRouter, HTTPException
 from requests import HTTPError
 from models.game import Game
-from services.steam_game_service import get_game_details_by_appid, get_game_details_by_name
+from services.steam_game_service import SteamGameService
 
 router = APIRouter()
+gameService = SteamGameService()
 
-@router.get("/game/{appid}", summary="Get game details by appid.")
+@router.get("/game/{appid}", summary="Get game details by appid.", response_model=Game)
 async def get_game_by_appid(appid: str) -> Game:
     game = None
 
     try:
-        game = get_game_details_by_appid(appid)
+        game = gameService.get_game_details_by_appid(appid)
     except HTTPError as e:
         raise HTTPException(status_code=404, detail="Game with id {appid} was not found.")
     
     return game
 
-@router.get("/game", summary="Get game details by name.")
+@router.get("/{appname}", summary="Get game details by name.", response_model=Game)
 async def get_game_by_name(appname: str) -> Game:
     game = None
 
     try:
-        game = get_game_details_by_name(appname)
+        game = gameService.get_game_details_by_name(appname)
     except HTTPError as e:
         raise HTTPException(status_code=404, detail="Game with name {appname} was not found.")
     
