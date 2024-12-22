017822dbd927e050c77befc9f5711ebc41acf95f
AdrianMarTej <adrian.martinez.t@opendeusto.es>
2024-12-22 13:05:14 +0100

    Run Games API server

--- steam-games-api/main.py
+++ steam-games-api/main.py
@@ -1,17 +1,23 @@
 from services.steam_game_service import SteamGameService
+from controller import app
 
-# Example usage
 if __name__ == "__main__":
-    steam_service = SteamGameService()
-    appid = 1245620
-    game = steam_service.get_game_details_by_appid(appid)
-    if game:
-        print(game)
+    import uvicorn
+
+    uvicorn.run(app, host="localhost", port=8000)
+
+# # Example usage
+# if __name__ == "__main__":
+#     steam_service = SteamGameService()
+#     appid = 1245620
+#     game = steam_service.get_game_details_by_appid(appid)
+#     if game:
+#         print(game)
     
-    steam_service = SteamGameService()
-    game_name = "Elden Ring"
-    game = steam_service.get_game_details_by_name(game_name)
+#     steam_service = SteamGameService()
+#     game_name = "Elden Ring"
+#     game = steam_service.get_game_details_by_name(game_name)
     
-    # Display game details if found
-    if game:
-        print(game.json())
+#     # Display game details if found
+#     if game:
+#         print(game.json())
