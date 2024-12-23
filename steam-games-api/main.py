df7de376185e94476e8e426d988053239bdd3934
AdrianMarTej <adrian.martinez.t@opendeusto.es>
2024-12-23 19:17:56 +0100

    Games API updated requirements and ip

--- steam-games-api/main.py
+++ steam-games-api/main.py
@@ -4,7 +4,7 @@ from controller import app
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 # # Example usage
 # if __name__ == "__main__":
